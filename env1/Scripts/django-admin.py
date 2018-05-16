d5fcf5935d5994766c9649d6a12bc6ff87743515
masatohs <masatohs@hotmail.com>
2018-05-17 08:55:04 +0900

    My Django Girls app, first commit

---
+++ env1/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\tsuge-m\AppData\Local\Programs\Python\Python36-32\env\env1\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
